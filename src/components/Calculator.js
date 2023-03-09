34df4cf0a6cc844ed15115a1a5e015152cdb42e8
Sokhapanhanith <panhanithsokah@yahoo.com>
2023-03-09 15:36:49 +1100

    commit

---
+++ src/components/Calculator.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import "../App.css";
+export const Calculator = () => {
+    const [number, setNumber] = useState(0);
+    const [prevnumber, setprevNumber] = useState(0);
+    const [total, setTotal] = useState(0);
+    return (
+        <>
+            <h1>Basic Calculator</h1>
+            <input className="add-text"
+            type="number"
+            value={number}
+            placeholder = "Please Enter the First Number!"
+            onChange={(e) => setNumber(e.target.value)}
+            />
+            <input className="add-text"
+            type="number"
+            value={prevnumber}
+            placeholder = "Please Enter the Second Number!"
+            onChange={(e) => setprevNumber(e.target.value)}
+            />
+            <div>
+            <button onClick={() => setTotal(+number + +prevnumber)} className="button-check">+</button>
+            <button onClick={() => setTotal((+number / +prevnumber))} className="button-check">Divide</button>
+            <button onClick={() => setTotal((+number * +prevnumber))} className="button-check">Multiplication</button>
+            <button onClick={() => setTotal((+number - +prevnumber))} className="button-check">Minus</button>
+            </div>
+            <p>Result : {total}</p>
+        </>
+    )
+}
